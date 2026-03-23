6e629052706cc4d697d3fac703a00a186707718e
Elvis Chege <elvischege98@gmail.com>
2026-03-23 10:58:43 +0300

    Finished javascript test 1

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+function calculateTax(amount) {
+    const taxRate = 0.1; // 10% tax
+    return amount * taxRate;
+}
+
+console.log(calculateTax(100));
+
+function convertToUpperCase(str) {
+    return str.toUpperCase();
+}
+
+console.log(convertToUpperCase("Hi there!"));
+
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2);
+}
+
+console.log(findMaximum(100, 10));
+
+function isPalindrome(str) {
+    return str === str.split('').reverse().join('');
+}
+
+console.log(isPalindrome("madam"));
 
 
+const discountPercentage = 0.2; 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
 
+    const discountAmount = (originalPrice * discountPercentage);
+    return originalPrice * discountPercentage;
+}
 
+console.log(calculateDiscountedPrice(100, 0.2));
+console.log(calculateDiscountedPrice(58, 0.2));
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// // This is required for the test to function properly  
+// module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
